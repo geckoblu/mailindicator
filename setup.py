c240f6df66a3fd49d713f836f7fa80c7fae86718
Alessio Piccoli <alepic@geckoblu.net>
2023-10-04 14:01:28 +0200

    setup.cfg

--- setup.py
+++ setup.py
@@ -1,33 +1,5 @@
 #!/usr/bin/python3
-"""
-  Author: Alessio Piccoli <alepic@geckoblu.net>
+from setuptools import setup
 
-  This script needs python-distutils-extra, an extension to the standard
-  distutils which provides i18n, icon support, etc.
-
-  https://launchpad.net/python-distutils-extra
-"""
-
-from distutils.version import StrictVersion
-
-try:
-    import DistUtilsExtra.auto
-except ImportError:
-    import sys
-    sys.stderr.write(
-            'To build mailindicator you need https://launchpad.net/python-distutils-extra\n')
-    sys.exit(1)
-
-assert StrictVersion(DistUtilsExtra.auto.__version__) >= '2.4', 'needs DistUtilsExtra.auto >= 2.4'
-
-DistUtilsExtra.auto.setup(
-    name='mailindicator',
-    version='0.1',
-    description="Monitors mailboxes for new mail.",
-    long_description="Monitors mailboxes for new mail.",
-    url="http://www.geckoblu.net/html/mailindicator.html",
-    license='GPL v3 or later',
-    author="Alessio Piccoli",
-    author_email="alepic@geckoblu.net",
-    packages=['mailindicator', 'mailindicator.sanction']
-)
+if __name__ == '__main__':
+    setup()
